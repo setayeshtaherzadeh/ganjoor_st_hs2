9f3be4f1bbbb9427ab0de6f2579b56d11527b57c
setayeshtaherzadeh <107579513+setayeshtaherzadeh@users.noreply.github.com>
2022-06-16 00:55:28 +0430

    uii ganjoor

---
+++ MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.ganjoor_st_hs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
